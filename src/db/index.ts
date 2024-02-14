90cd3472dea16e8e3fa3692324f2996aa9cede0a
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-02-15 01:27:39 +0800

    feat(db): add nullable constraint to suara_paslon columns

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,11 @@
-import { wilayah } from "@/db/schema";
+import { ppwpTps, wilayah } from "@/db/schema";
 import { env } from "@/utils/env";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 export const dbSchema = {
   wilayah,
+  ppwpTps,
 };
 
 export const migrationClient = drizzle(postgres(env.DATABASE_URL, { max: 1 }));
---
+++ src/db/migrations/0004_marvelous_mathemanic.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "ppwp_tps" ALTER COLUMN "suara_paslon_1" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "ppwp_tps" ALTER COLUMN "suara_paslon_2" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "ppwp_tps" ALTER COLUMN "suara_paslon_3" DROP NOT NULL;
