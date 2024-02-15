849a73285e21d25a1262684111ef2538a63430f4
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-02-15 09:41:13 +0800

    feat(db): rename columns in ppwp_tps table

---
+++ src/db/migrations/0005_nifty_russian.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "ppwp_tps" RENAME COLUMN "hasil_1" TO "chasil_hal_1";--> statement-breakpoint
+ALTER TABLE "ppwp_tps" RENAME COLUMN "hasil_2" TO "chasil_hal_2";--> statement-breakpoint
+ALTER TABLE "ppwp_tps" RENAME COLUMN "hasil_3" TO "chasil_hal_3";
