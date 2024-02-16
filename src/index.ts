54e054d15d72a6bfd3ffcbffeb9062d732972c9d
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-02-16 15:33:26 +0800

    feat: refactor command handling in index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,22 +6,33 @@ import {
 import { getWilayah } from "@/actions/get-wilayah";
 
 const args = process.argv;
-const command = args[2];
+const election = args[2] as Election;
+const command = args[3];
 
-switch (command) {
-  case "get-wilayah":
-    await getWilayah();
+type Election = "pilpres" | "dpr" | "dpd" | "dprd-prov" | "dprd-kabkot";
+
+switch (election) {
+  case "pilpres":
+    switch (command) {
+      case "get-wilayah":
+        await getWilayah();
+        break;
+      case "get-tps-detail":
+        await getTpsDetail();
+        break;
+      case "get-tps-detail-2":
+        await getTpsDetailV2();
+        break;
+      case "update-tps-detail":
+        await updateTpsDetail();
+        break;
+      default:
+        console.log("Command not recognized");
+        break;
+    }
     break;
-  case "get-tps-detail":
-    await getTpsDetail();
-    break;
-  case "get-tps-detail-2":
-    await getTpsDetailV2();
-    break;
-  case "update-tps-detail":
-    await updateTpsDetail();
-    break;
-  default:
-    console.log("Command not recognized");
-    break;
+  case "dpr":
+  case "dpd":
+  case "dprd-prov":
+  case "dprd-kabkot":
 }
