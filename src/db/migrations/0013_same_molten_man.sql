aae2897ceb4f0b8b04cb44f724cdfacc25e3adb2
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-02-16 18:57:54 +0800

    feat(db): add indexes to "wilayah" table for faster querying

---
+++ src/db/migrations/0013_same_molten_man.sql
@@ -0,0 +1,6 @@
+CREATE INDEX IF NOT EXISTS "tingkat_index" ON "wilayah" ("tingkat");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "is_fetched_presiden_index" ON "wilayah" ("is_fetched_presiden");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "is_fetched_dpr_index" ON "wilayah" ("is_fetched_dpr");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "is_fetched_dpd_index" ON "wilayah" ("is_fetched_dpd");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "is_fetched_dprd_provinsi_index" ON "wilayah" ("is_fetched_dprd_provinsi");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "is_fetched_dprd_kabupaten_kota_index" ON "wilayah" ("is_fetched_dprd_kabupaten_kota");
