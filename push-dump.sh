936ff329c842e24ec75e0185dc7c5e694286665a
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-02-19 19:53:37 +0800

    revert

--- push-dump.sh
+++ push-dump.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
-
-# Step 1: Open `dump` folder
-cd dump || { echo "Error: dump folder not found"; exit 1; }
-
-# Step 2: Scan files number
-file_count=$(ls -1 | wc -l)
-echo "Total number of files: $file_count"
-
-# Step 3: Chunk group to every 100 size
-chunk_size=100
-group_count=$(( ($file_count + $chunk_size - 1) / $chunk_size )) # Round up division
-echo "Number of groups: $group_count"
-
-# Step 4: Loop group, to git add "this all files every group", git commit, git push origin HEAD:main
-for ((i = 0; i < $group_count; i++)); do
-    start=$((i * chunk_size))
-    end=$((start + chunk_size - 1))
-    files=($(ls | sed -n "$((start + 1)),$((end + 1))p"))
-
-    # Add files to git
-    git add "${files[@]}"
-
-    # Commit changes
-    git commit -m "Added files from group $((i + 1))"
-
-    # Push changes to origin
-    git push origin HEAD:main
+cd dump/
+# Tentuin jumlah file per commit
+files_per_commit=5
+# Ambil list modified files
+modified_files=$(git status --porcelain | grep '^ M' | cut -d ' ' -f 3)
+# Hitung jumlah modified files
+total_files=$(echo "$modified_files" | wc -l)
+# Hitung jumlah commit yang dibutuhkan
+total_commits=$((total_files / files_per_commit))
+if [ $((total_files % files_per_commit)) -ne 0 ]; then
+    total_commits=$((total_commits + 1))
+fi
+# Loop untuk commit dan push setiap 100 file
+for ((i = 0; i < total_commits; i++)); do
+    start_index=$((i * files_per_commit))
+    end_index=$((start_index + files_per_commit - 1))
+    if [ $end_index -ge $total_files ]; then
+        end_index=$((total_files - 1))
+    fi
+    commit_files=$(echo "$modified_files" | sed -n "$((start_index + 1)),$((end_index + 1))p")
+    git add $commit_files
+    git commit -m "Commit $((i + 1)) dari $total_commits"
 done
+
+git push origin HEAD:main
