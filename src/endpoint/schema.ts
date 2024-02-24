47a4be59ff57c0d19491634d57819508ec7633c4
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-02-25 05:23:10 +0800

    feat: add Penghitungan Suara Susulan

--- src/endpoint/schema.ts
+++ src/endpoint/schema.ts
@@ -2,8 +2,9 @@ import { z } from "zod";
 
 export const psuEnum = z.enum([
   "Reguler",
-  "Pemungutan Suara Ulang",
   "Penghitungan Suara Ulang",
+  "Penghitungan Suara Susulan",
+  "Pemungutan Suara Ulang",
   "Pemungutan Suara Susulan",
   "Pemungutan Suara Lanjutan",
 ]);
